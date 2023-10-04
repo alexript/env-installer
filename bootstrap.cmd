8fa8256fe6cc7ad7a970a0310008bfa552f0fa19
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-10-04 16:51:02 +0300

    bootstrap nushell

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -2,4 +2,13 @@
 set RUSTUP_HOME=%~dp0.rustup
 set CARGO_HOME=%~dp0.cargo
 set PATH=%CARGO_HOME%\bin;%PATH%
+
+git submodule update --init --recursive
+
 curl --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
+
+cd apps\nushell
+cargo install --path . 
+cargo clean
+cd ..\..
+
