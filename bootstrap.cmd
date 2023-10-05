73f10d58efa06ed62603b419fd02ad0646722edb
Alex 'Ript' Malyshev <alexript@gmail.com>
2023-10-05 15:15:47 +0300

    bootstrap tinycc

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,21 +1,30 @@
 @echo off
-set RUSTUP_HOME=%~dp0.rustup
-set CARGO_HOME=%~dp0.cargo
-set V_HOME=%~dp0apps\v
 
-set PATH=%CARGO_HOME%\bin;%V_HOME%;%PATH%
+set _ENVROOT=%~dp0
+set RUSTUP_HOME=%_ENVROOT%.rustup
+set CARGO_HOME=%_ENVROOT%.cargo
+set V_HOME=%_ENVROOT%apps\v
+set TCC_HOME=%_ENVROOT%tcc
+
+set PATH=%CARGO_HOME%\bin;%TCC_HOME%;%V_HOME%;%PATH%
 
 git submodule update --init --recursive
 
 curl --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 
-cd apps\nushell
+cd %_ENVROOT%apps\nushell
 cargo install --path . 
 rem cargo clean
-cd ..\..
 
-cd apps\v
-make.bat
-cd ..
+cd %_ENVROOT%apps\v
+call make.bat
 
+cd %_ENVROOT%apps\tinycc
+cd win32
+call build-tcc.bat -c %_ENVROOT%apps\v\thirdparty\tcc\tcc -i %_ENVROOT%\tcc
+call build-tcc.bat -c %_ENVROOT%\tcc\tcc -i %_ENVROOT%\tcc
+call build-tcc.bat -c %_ENVROOT%\tcc\tcc -i %_ENVROOT%\tcc
+
+
+cd %_ENVROOT%
 nu --env-config bootstrap\nushell\env.nu --config bootstrap\nushell\config.nu setup.nu
--- shell.cmd
+++ shell.cmd
@@ -1,9 +1,10 @@
 @echo off
-set RUSTUP_HOME=%~dp0.rustup
-set CARGO_HOME=%~dp0.cargo
-set V_HOME=%~dp0apps\v
+set _ENVROOT=%~dp0
+set RUSTUP_HOME=%_ENVROOT%.rustup
+set CARGO_HOME=%_ENVROOT%.cargo
+set V_HOME=%_ENVROOT%apps\v
+set TCC_HOME=%_ENVROOT%tcc
 
-set PATH=%CARGO_HOME%\bin;%V_HOME%;%PATH%
-
+set PATH=%CARGO_HOME%\bin;%TCC_HOME%;%V_HOME%;%PATH%
 
 nu --env-config .config\nushell\env.nu --config .config\nushell\config.nu
